c61c6efef9cdbfd0081fb288052f1a1d91f1a367
Alex Cline <rev.a.r.cline@gmail.com>
2020-12-10 15:37:25 -0600

    commit tree 9e4faaac38bb656e1add7b1f1d46a8f6e57cb9d8

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
-# Your code goes here!
+class Anagram
+  # matches word to matches
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(matches)
+    matches.select { |entry| entry.chars.sort == @word.chars.sort }
+  end
+end
